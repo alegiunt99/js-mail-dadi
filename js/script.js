5b582076fcddb9021a53e0da4be3bdba6bad1d24
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-09 16:14:31 +0100

    Ho aggiunto l'input email con il pulsante

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@ console.log("JS OK")
 // devo controllare che sia all'interno della lista che può accedere
 // devo far spuntare un messaggio in base all'esito del controllo
 
+const emailContainer = document.querySelector('.container');
+
+const emailInput = document.querySelector('#email-input');
+
+const emailButton = document.querySelector('#email-button');
+

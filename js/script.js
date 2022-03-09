f07db39b6dabbf39ebc6c5276ffb0f9b3c18f900
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-09 15:51:46 +0100

    Init

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log("JS OK")
+
+// EMAIL
+// Devo richiedere un'email all'utente
+// devo controllare che sia all'interno della lista che può accedere
+// devo far spuntare un messaggio in base all'esito del controllo
+
